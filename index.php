9515739f67e2084523d8232902b68e51d2cc0c3b
ahmed alsarem <ahmedjameelalsarem@gmail.com>
2025-10-08 00:23:13 +0300

    🔧 إصلاح index.php
    - استبدال النافيجيشن المكرر بـ include navigation.php
    - تحسين تنظيم الكود وتجنب التكرار
    - ضمان استخدام navigation.php الموحد

--- index.php
+++ index.php
@@ -40,24 +40,7 @@ $user_name = isset($_SESSION['user_name']) ? $_SESSION['user_name'] : "زائر"
 <body>
 
 <!-- Navigation -->
-<nav>
-    <div class="container">
-        <h1><a href="home.php" style="text-decoration: none; color: inherit;">SR-Techno</a></h1>
-        <ul>
-            <li><a href="home.php">الرئيسية</a></li>
-            <li><a href="product.php">المنتجات</a></li>
-            <li><a href="cart.php">السلة</a></li>
-            <li><a href="checkout.php">الدفع</a></li>
-            <?php if(isset($_SESSION['user_id'])): ?>
-                <li><span class="user-info">مرحباً، <?php echo htmlspecialchars($user_name); ?></span></li>
-                <li><a href="logout.php" class="logout-btn">تسجيل الخروج</a></li>
-            <?php else: ?>
-                <li><a href="login.php">تسجيل الدخول</a></li>
-                <li><a href="register.php">تسجيل جديد</a></li>
-            <?php endif; ?>
-        </ul>
-    </div>
-</nav>
+<?php include 'navigation.php'; ?>
 
 <!-- Page Content -->
 <main>
